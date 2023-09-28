acfa4911a0d09cc5c4677d01b05db29287be4fd6
KogeyR <134587760+KogeyR@users.noreply.github.com>
2023-09-28 16:46:46 +0200

    admin done

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Application;
+use App\Entity\Candidat;
+use App\Entity\Category;
+use App\Entity\Client;
+use App\Entity\Experience;
+use App\Entity\Gender;
+use App\Entity\Type;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -51,7 +59,15 @@ public function __construct(private AdminUrlGenerator $adminUrlGenerator) {
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Offer', 'fa-brands fa-buffer');
+        yield MenuItem::linkToCrud('Application', 'fa-regular fa-clipboard', Application::class);
+        yield MenuItem::linkToCrud('Candidat', 'fa-solid fa-inbox', Candidat::class);
+        yield MenuItem::linkToCrud('Client', 'fa-solid fa-list', Client::class);
+        yield MenuItem::linkToCrud('Category','fa-solid fa-user-minus', Category::class);
+        yield MenuItem::linkToCrud('Experience','fa-solid fa-school', Experience::class);
+        yield MenuItem::linkToCrud('Gender','fa-solid fa-person-half-dress', Gender::class);
+        yield MenuItem::linkToCrud('Type','fa-solid fa-filter', Type::class);
+        yield MenuItem::linkToCrud('user','fa-solid fa-users', User::class);
+        
     }
 }
