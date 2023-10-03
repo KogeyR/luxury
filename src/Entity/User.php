a782e6d9ad757ed5922a7a84ac66ffa963842f80
KogeyR <134587760+KogeyR@users.noreply.github.com>
2023-10-03 09:23:51 +0200

    condition login/logout

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -105,6 +105,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __toString(): string
     {
         return $this->email;
+       
     }
 
     public function getCandidat(): ?Candidat
@@ -128,4 +129,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    
+
 }
