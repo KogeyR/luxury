1982a609d999afce23ec51a8d1e62c36d6fb1322
KogeyR <134587760+KogeyR@users.noreply.github.com>
2023-10-09 14:03:40 +0200

    candidat debugged

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
